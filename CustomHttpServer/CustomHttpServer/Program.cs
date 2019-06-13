1507aaac81030f762c2ba32be95dff91e5cde90c
Zeynalli Farid <faridaz@code.edu.az>
2019-06-13 12:36:16 -0700

    commit

--- CustomHttpServer/CustomHttpServer/Program.cs
+++ CustomHttpServer/CustomHttpServer/Program.cs
@@ -1,18 +1,161 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace CustomHttpServer
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting service on port 8080");
-            HttpService service = new HttpService(8080);
-            service.Start();
+            SimpleListenerExample(new string[] { "http://127.0.0.1:8080/" });
         }
+        // This example requires the System and System.Net namespaces.
+        public static void SimpleListenerExample(string[] prefixes)
+        {
+            bool isrunning = true;
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                return;
+            }
+            // URI prefixes are required,
+            // for example "http://contoso.com:8080/index/".
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+
+            // Create a listener.
+            HttpListener listener = new HttpListener();
+            // Add the prefixes.
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            listener.Start();
+            Console.WriteLine("Listening...");
+            System.IO.Stream output = null;
+            while (isrunning)
+            {
+                try
+                {
+                    // Note: The GetContext method blocks while waiting for a request. 
+                    HttpListenerContext context = listener.GetContext();
+                    string url = context.Request.RawUrl;
+                    if (url == "/" || url == "/favicon.ico") url = "\\index.html";
+                    string fileExtension = url.Substring(url.LastIndexOf("."));
+                    HttpListenerRequest request = context.Request;
+                    // Obtain a response object.
+                    HttpListenerResponse response = context.Response;
+                    // Construct a response.
+                    //string responseString = "<HTML><BODY> Hello dsfdsfsfsffsdfFarid!</BODY></HTML>";
+                    //int length = responseString.Length;
+                    byte[] buffer = GetView(url);
+                    //byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                    // Get a response stream and write the response to it.
+                    response.ContentLength64 = buffer.Length;
+                    response.AddHeader("Content-type", _mimeTypeMappings[fileExtension]);
+                    output = response.OutputStream;
+
+                    output.Write(buffer, 0, buffer.Length);
+                    // You must close the output stream.
+                }
+                catch (Exception e)
+                {
+                    isrunning = false;
+                    Console.WriteLine(e);
+                }
+                finally
+                {
+                    if (output != null)
+                        output.Close();
+                }
+            }
+            listener.Stop();
+        }
+
+        private static byte[] GetView(string url)
+        {
+            string filepath = Environment.CurrentDirectory + "\\Logistics" + url;
+            FileInfo fi = new FileInfo(filepath);
+            FileStream fs = fi.OpenRead();
+            BinaryReader br = new BinaryReader(fs);
+            byte[] bytes = new byte[fs.Length];
+            br.Read(bytes, 0, bytes.Length);
+            fs.Close();
+            return bytes;
+        }
+
+        private static IDictionary<string, string> _mimeTypeMappings = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) {
+        {".asf", "video/x-ms-asf"},
+        {".asx", "video/x-ms-asf"},
+        {".avi", "video/x-msvideo"},
+        {".bin", "application/octet-stream"},
+        {".cco", "application/x-cocoa"},
+        {".crt", "application/x-x509-ca-cert"},
+        {".css", "text/css"},
+        {".deb", "application/octet-stream"},
+        {".der", "application/x-x509-ca-cert"},
+        {".dll", "application/octet-stream"},
+        {".dmg", "application/octet-stream"},
+        {".ear", "application/java-archive"},
+        {".eot", "application/octet-stream"},
+        {".exe", "application/octet-stream"},
+        {".flv", "video/x-flv"},
+        {".gif", "image/gif"},
+        {".hqx", "application/mac-binhex40"},
+        {".htc", "text/x-component"},
+        {".htm", "text/html"},
+        {".html", "text/html"},
+        {".ico", "image/x-icon"},
+        {".img", "application/octet-stream"},
+        {".iso", "application/octet-stream"},
+        {".jar", "application/java-archive"},
+        {".jardiff", "application/x-java-archive-diff"},
+        {".jng", "image/x-jng"},
+        {".jnlp", "application/x-java-jnlp-file"},
+        {".jpeg", "image/jpeg"},
+        {".jpg", "image/jpeg"},
+        {".js", "application/x-javascript"},
+        {".mml", "text/mathml"},
+        {".mng", "video/x-mng"},
+        {".mov", "video/quicktime"},
+        {".mp3", "audio/mpeg"},
+        {".mpeg", "video/mpeg"},
+        {".mpg", "video/mpeg"},
+        {".msi", "application/octet-stream"},
+        {".msm", "application/octet-stream"},
+        {".msp", "application/octet-stream"},
+        {".pdb", "application/x-pilot"},
+        {".pdf", "application/pdf"},
+        {".pem", "application/x-x509-ca-cert"},
+        {".pl", "application/x-perl"},
+        {".pm", "application/x-perl"},
+        {".png", "image/png"},
+        {".prc", "application/x-pilot"},
+        {".ra", "audio/x-realaudio"},
+        {".rar", "application/x-rar-compressed"},
+        {".rpm", "application/x-redhat-package-manager"},
+        {".rss", "text/xml"},
+        {".run", "application/x-makeself"},
+        {".sea", "application/x-sea"},
+        {".shtml", "text/html"},
+        {".sit", "application/x-stuffit"},
+        {".swf", "application/x-shockwave-flash"},
+        {".tcl", "application/x-tcl"},
+        {".tk", "application/x-tcl"},
+        {".txt", "text/plain"},
+        {".war", "application/java-archive"},
+        {".wbmp", "image/vnd.wap.wbmp"},
+        {".wmv", "video/x-ms-wmv"},
+        {".xml", "text/xml"},
+        {".xpi", "application/x-xpinstall"},
+        {".zip", "application/zip"}
+        };
+
     }
 }
